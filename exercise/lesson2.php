99dd439b7861049b28bcf73c4f9e38ab362a0b37
Yuta-Okugawa <yuta.okugawa@dragon.jp>
2015-06-12 19:05:32 +0900

    #2

---
+++ exercise/lesson2.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>練習問題２</title>
+    </head>
+    <body>
+        <?php
+           error_reporting(-1);
+
+            $max = 9; //最大値
+            $half = floor($max/2); //折り返し値-１
+            $num = "&nbsp;&nbsp;"; //数字前のスペースの初期値
+            $sp = "&nbsp;&nbsp;"; //一回で増えるスペースの量
+            $exSp = strlen($sp); //減らすスペースの文字数
+
+            if(isset($_GET['index'])){
+                for($i = 1; $i <= $max; $i++){
+                    if($i < $half){
+                        echo "$i"."<br>"."$num";
+                        $num = "$num"."$sp";
+                    } elseif($i == $half){
+                        echo "$i"."<br>"."$num";
+                    } else {
+                        $sl = strlen("$num");
+                        $sl = $sl - $exSp;
+                        $num = substr($num, 0, $sl);
+                        echo "$i"."<br>"."$num";
+                    }
+                }
+
+            } else {
+                echo "<form action='test.php' method='get'>
+                    <input type='submit' name='index' value='実行'>
+                </form>";
+            }
+        ?>
+    </body>
+</html>
