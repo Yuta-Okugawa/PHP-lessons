f6499c2fbdc1f7728d2c351e7c7699183cfb41e0
Yuta-Okugawa <yuta.okugawa@dragon.jp>
2015-06-16 14:26:13 +0900

    #9

---
+++ exercise/problem1/select_number.php
@@ -0,0 +1,27 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['num'])){
+        $_SESSION['num'] = $_POST['num'];
+    }
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>問題1</title>
+    </head>
+    <body>
+         <p>秘密の数字を当ててください。</p>
+        <form action='result_number.php' method='post'>
+            <script>
+                document.write("<SELECT name='selectNum'>");
+                for(i=1; i<=20; i++) {
+                    document.write("<OPTION value='"+i+"'>"+i+"</OPTION>");
+                };
+                document.write("</select>");        
+            </script>
+            <input type='submit' value='決定' name='index'>
+            </select>
+        </form>
+    </body>
+</html>
