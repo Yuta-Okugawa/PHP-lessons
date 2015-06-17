41c24a3d4d9127d258b79d2f068a061d11b9f4b0
Yuta-Okugawa <yuta.okugawa@dragon.jp>
2015-06-17 12:36:23 +0900

    作業中

---
+++ exercise/practice3/numCheck.php
@@ -0,0 +1,26 @@
+<?php
+    $result = "九桁の数字を入力してください。";
+    if(isset($_POST['int'])){
+        if(mb_strlen($_POST['int']) != 9){
+            $result = "桁数が間違っています。"; 
+        } else {
+            $array = str_split($_POST['int'], 1);
+            //var_dump($array);
+            $result = "九つの数字の合計は「".array_sum($array)."」です。";
+        }
+    }
+?>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>問題3　足し算</title>
+    </head>
+    <body>
+        <form action="numCheck.php" method="post">
+            <?php echo $result; ?><br>
+            <input type="text" name="int">
+            <input type="submit" value="check">
+        </form>
+    </body>
+</html>
