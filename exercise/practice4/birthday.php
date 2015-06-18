91c20f50c496a798badf739c2a16e2bfd4adc8b6
Yuta-Okugawa <yuta.okugawa@dragon.jp>
2015-06-18 10:16:54 +0900

    #22

--- exercise/practice4/birthday.php
+++ exercise/practice4/birthday.php
@@ -1,13 +1,21 @@
 <?php
     $result = "生年月日を入力してください ※1980年1月1日の場合（19800101）と入力してください。";
     if(isset($_POST['birth'])){
-        if(!is_numeric($_POST['birth'])){
+        if(!ctype_digit($_POST['birth'])){
             $result = "数字を半角で入力してください。";
         } elseif(mb_strlen($_POST['birth']) != 8){
             $result = "桁数が間違っています。"; 
         } else {
-            $now = date("Ymd");
-            $result = "年齢は".floor(($now-($_POST['birth'])) / 10000)."歳です。";
+            $year = substr($_POST['birth'], 0, 4);
+            $month = substr($_POST['birth'], 4, 2);
+            $day = substr($_POST['birth'], 6, 2);
+            
+            if (!checkdate($month, $day, $year)){
+                $result = "正しい日付を入力してください。";
+            } else {
+                $now = date("Ymd");
+                $result = "年齢は".floor(($now-($_POST['birth'])) / 10000)."歳です。";
+            }
         }
     }
 ?>
