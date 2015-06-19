103545c5258187d507323d92548cdcb4504b49ee
Yuta-Okugawa <yuta.okugawa@dragon.jp>
2015-06-19 20:09:53 +0900

    #26

--- exercise/practice7/primeNumber.php
+++ exercise/practice7/primeNumber.php
@@ -6,31 +6,39 @@
     $max = 100;
     $total = 0;
     $message = $min."~".$max."までの素数を除いた数字を足す";
-
+ 
     if(isset($_POST['add'])){
         // $min~$maxまでの足し算を繰り返す
         for($i = $min ; $i <= $max; $i++){
-            // 
             $count = 0;
-            for($j = 1; $j <= $i; $j++){
-                
-                if($i % $j == 0){
-                    $count = $count + 1;  
-                }
-            }
-            if($count == 2){
-                if($i == 2){
-                    $i = $i +2;
-                } else {
-                    $i += 1;
-                }
-            }
+            prime($i,$count);
             $total = $total + $i;
         }
         
         $message = $min."~".$max."までの素数を除いた合計は".$total."です。";
     }
 
+    // 素数を判定して飛ばす関数
+    function prime(&$i,&$count){
+        // 1から$iまでの数で$1を剰余算
+        for($j = 1; $j <= $i; $j++){
+            // わり切れた回数をカウント
+            if($i % $j == 0){
+                $count = $count + 1;  
+            }
+        }
+        // わり切れた回数が二回なら素数
+        if($count == 2){
+            // 2は次が素数なので二つ飛ばす
+            if($i == 2){
+                $i = $i +2;
+            // 他は一つ飛ばす
+            } else {
+                $i += 1;
+            }
+            
+        }
+    }
     
 ?>
 <!doctype html>
