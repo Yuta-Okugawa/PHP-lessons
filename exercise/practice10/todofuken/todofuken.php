6ade4ffae07f9a992173fceca3dd5d7e506b9b58
Yuta-Okugawa <yuta.okugawa@dragon.jp>
2015-06-25 16:54:32 +0900

    #40　

--- exercise/practice10/todofuken/todofuken.php
+++ exercise/practice10/todofuken/todofuken.php
@@ -1,45 +1,34 @@
 <?php
     error_reporting(0);
     require_once("pref_conf.php");
-
-    $resArray ="";
     
+    // 初期メッセージ
     $message = "アルファベット1文字を入力してください。";
-    if(isset($_POST['str'])){
-        if(!ctype_alpha($_POST['str'])){
-            $message = "アルファベットを半角で入力してください。";
-        } elseif(mb_strlen($_POST['str']) != 1){
-            $message = "文字数は1文字入力してください。"; 
-        } else {
-            $message = $_POST['str']."で始まる都道府県";
-            $searchKey = mb_strtolower($_POST['str']);
-            $resArray = array();
-            $i = 0;
-            foreach ($prefectures as $key => $value){
-                if($value == $searchKey){
-                    $resArray[$i] = $key;
-                    $i++;
-                }
-            }
+    if(isset($_POST['alpha'])){
+        
+        // False時のメッセージ
+        $message = "アルファベットを半角で入力してください。";
+        // バリデーションチェック
+        if(ctype_alpha($_POST['alpha'])){
+            // 大文字だった場合小文字へ変換
+            $alpha = strtolower($_POST['alpha']);
+            $message = $_POST['alpha']."で始まる都道府県";
         }
     }
     
 ?>
-
-<!doctype html>
+<!DOCTYPE html>
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title>問題１０： 当する都道府県名をすべて表示</title>
+        <meta charset="utf-8">
+        <title>問題10:該当する都道府県名をすべて表示</title>
     </head>
     <body>
+        <?php echo $message; ?><br>
+        <?php resPrefs($prefectures,$alpha); ?><br>
         <form action="todofuken.php" method="post">
-            <?php echo $message; ?><br>
-            <?php resPlef($resArray); ?><br>
-            <input type="text" size="1" maxlength="1" name="str">
-            <input type="submit" value="変換">
+            <input type="text" size="1" maxlength="1" name="alpha">
+            <input type="submit" value="表示">
         </form>
-
-
     </body>
 </html>
