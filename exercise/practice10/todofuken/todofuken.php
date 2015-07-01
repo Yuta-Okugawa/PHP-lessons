1b5713f65210e4324a60b4f281fb5f435fcff73d
Yuta-Okugawa <yuta.okugawa@dragon.jp>
2015-07-01 16:19:47 +0900

    #40

--- exercise/practice10/todofuken/todofuken.php
+++ exercise/practice10/todofuken/todofuken.php
@@ -1,34 +1,35 @@
 <?php
-    error_reporting(0);
-    require_once("pref_conf.php");
-    
-    // 初期メッセージ
-    $message = "アルファベット1文字を入力してください。";
-    if(isset($_POST['alpha'])){
-        
-        // False時のメッセージ
-        $message = "アルファベットを半角で入力してください。";
-        // バリデーションチェック
-        if(ctype_alpha($_POST['alpha'])){
-            // 大文字だった場合小文字へ変換
-            $alpha = strtolower($_POST['alpha']);
-            $message = $_POST['alpha']."で始まる都道府県";
-        }
+error_reporting(-1);
+require_once("pref_conf.php");
+
+// 表示するメッセージの設定とバリデーションチェック
+$message = "検索する地域を選択して、都道府県名を英字で入力してください。";
+if(isset($_POST['str'])){
+    // エラー時のメッセージ
+    $message = "英字を半角で入力してください。";
+    // 小文字のアルファベットか空文字なら通す
+    if(ctype_alpha($_POST['str']) || $_POST['str'] == ""){
+        $message = "検索結果";
     }
-    
+}
+
 ?>
+
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
-        <title>問題10:該当する都道府県名をすべて表示</title>
+        <title>問題10：該当する都道府県を表示する</title>
     </head>
     <body>
-        <?php echo $message; ?><br>
-        <?php resPrefs($prefectures,$alpha); ?><br>
         <form action="todofuken.php" method="post">
-            <input type="text" size="1" maxlength="1" name="alpha">
-            <input type="submit" value="表示">
+            <?php makeSelect($area); ?>
+            <input type="text" size="1" value="<?php if(isset($_POST['str'])){echo $_POST['str'];}?>"  name="str">
+            <br>
+            <input type="submit">
         </form>
+        <p><?php echo $message; ?></p>
+        <hr>
+        <p><?php searchResult($respArea,$readList,$prefList)?></p>
     </body>
 </html>
